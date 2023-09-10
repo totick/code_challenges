398b68e9c399746ed3cec8cd71d4fb1c6ebcde7c
John Lundgren <john.paul.lundgren@gmail.com>
2023-09-10 09:25:42 +0200

    Tests for best time sell stock.

--- easy/best_time_to_sell_stock/best_time_to_sell_stock.test.js
+++ easy/best_time_to_sell_stock/best_time_to_sell_stock.test.js
@@ -1,6 +1,25 @@
-import { max_profit } from './best_time_to_sell_stock.js';
+import { maxProfit } from './best_time_to_sell_stock.js';
 import { it, expect } from 'vitest';
 
-it('should', () => {
-  expect(1).toBe(1);
+it('should return 0 when there is no profit to make', () => {
+  //Arrange
+  const input = [7, 6, 4, 3, 1];
+  const expectedResult = 0;
+
+  //Act
+  const result = maxProfit(input);
+
+  //Assert
+  expect(result).toBe(expectedResult);
 });
+
+it.each([[[7, 1, 5, 3, 6, 4], 5]])(
+  'should return the value of the highest profit',
+  (input, expectedResult) => {
+    //Act
+    const result = maxProfit(input);
+
+    //Assert
+    expect(result).toBe(expectedResult);
+  }
+);
