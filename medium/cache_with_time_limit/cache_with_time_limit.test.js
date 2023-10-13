ea6cff804e6b5ce8c4e449fda3f9ee98ff9be036
John Lundgren <john.paul.lundgren@gmail.com>
2023-10-13 07:37:36 +0200

    Test for checking after no un-expired keys in cache.

---
+++ medium/cache_with_time_limit/cache_with_time_limit.test.js
@@ -0,0 +1,40 @@
+/*
+Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+
+The class has three public methods:
+
+set(key, value, duration): 
+  accepts an integer key, an integer value, and a duration in milliseconds. 
+  Once the duration has elapsed, the key should be inaccessible. 
+  The method should return true if the same un-expired key already exists and false otherwise. 
+  Both the value and duration should be overwritten if the key already exists.
+
+get(key): 
+  if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+
+count(): 
+  returns the count of un-expired keys.
+*/
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
+
+import { it, expect } from 'vitest';
+import { TimeLimitedCache } from './cache_with_time_limit';
+
+it('should return -1 if no un-expired key exists', () => {
+  //Arrange
+  const key = 1;
+  const cache = new TimeLimitedCache();
+  const expectedResult = -1;
+
+  //Act
+  const result = cache.get(key);
+
+  //Assert
+  expect(result).toBe(expectedResult);
+});
