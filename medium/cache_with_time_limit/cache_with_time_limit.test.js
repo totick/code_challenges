cd0d8aa62cae15aef9a560b079595404511dc9ca
John Lundgren <john.paul.lundgren@gmail.com>
2023-10-14 08:24:13 +0200

    Expiration and count tests.

--- medium/cache_with_time_limit/cache_with_time_limit.test.js
+++ medium/cache_with_time_limit/cache_with_time_limit.test.js
@@ -26,7 +26,13 @@ count():
 import { it, expect } from 'vitest';
 import { TimeLimitedCache } from './cache_with_time_limit';
 
-it('should return -1 if no un-expired key exists', () => {
+const wait = async (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+it('should return -1 when key does not exist', () => {
   //Arrange
   const key = 1;
   const cache = new TimeLimitedCache();
@@ -39,7 +45,7 @@ it('should return -1 if no un-expired key exists', () => {
   expect(result).toBe(expectedResult);
 });
 
-it('should return false when adding a key that does not already exists', () => {
+it('should return false when adding a new key', () => {
   //Arrange
   const key = 1;
   const value = 0;
@@ -84,5 +90,66 @@ it('should overwrite existing key with new value and response should be true', (
 
   //Assert
   expect(response).toBeTruthy();
+  expect(result).toBe(expectedResult);
+});
+
+it('should return -1 when getting expired key', async () => {
+  //Arrange
+  const key = 1;
+  const value = 2;
+  const duration = 100;
+  const expectedResult = -1;
+  const cache = new TimeLimitedCache();
+
+  //Act
+  cache.set(key, value, duration);
+  await wait(300);
+  const result = cache.get(key);
+
+  //Assert
+  expect(result).toBe(expectedResult);
+});
+
+it('should return correct value when getting it from an un-expired key', async () => {
+  //Arrange
+  const key = 1;
+  const value = 2;
+  const duration = 3000;
+  const expectedResult = value;
+  const cache = new TimeLimitedCache();
+
+  //Act
+  cache.set(key, value, duration);
+  await wait(1000);
+  const result = cache.get(key);
+
+  //Assert
+  expect(result).toBe(expectedResult);
+});
+
+it('should return correct count of un-expired keys', async () => {
+  //Arrange
+  const key1 = 1;
+  const value1 = 2;
+  const key2 = 2;
+  const value2 = 3;
+  const key3 = 3;
+  const value3 = 4;
+  const key4 = 4;
+  const value4 = 5;
+  const duration1 = 300;
+  const duration2 = 1500;
+  const expectedResult = 2;
+  const cache = new TimeLimitedCache();
+
+  //Act
+  cache.set(key1, value1, duration1);
+  cache.set(key2, value2, duration2);
+  cache.set(key3, value3, duration1);
+  cache.set(key4, value4, duration2);
+  await wait(700);
+  const result = cache.count();
+
+  //Assert
   expect(result).toBe(expectedResult);
 });
