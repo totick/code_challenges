242d3c207b9f2a374bc35480163069fe690ea868
John Lundgren <john.paul.lundgren@gmail.com>
2023-10-15 06:23:07 +0200

    Bugfix: Remove recreation of timeouts that remove keys from cache.

--- medium/cache_with_time_limit/cache_with_time_limit.js
+++ medium/cache_with_time_limit/cache_with_time_limit.js
@@ -11,8 +11,12 @@ export const TimeLimitedCache = function () {
  */
 TimeLimitedCache.prototype.set = function (key, value, duration) {
   const response = this.get(key) === -1 ? false : true;
-  this.cache[key] = value;
-  setTimeout(() => delete this.cache[key], duration);
+
+  if (response) clearTimeout(this.cache[key].timeoutId);
+
+  this.cache[key] = { value };
+  const timeoutId = setTimeout(() => delete this.cache[key], duration);
+  this.cache[key].timeoutId = timeoutId;
 
   return response;
 };
@@ -23,7 +27,7 @@ TimeLimitedCache.prototype.set = function (key, value, duration) {
  * @return {number} associated value with key
  */
 TimeLimitedCache.prototype.get = function (key) {
-  return this.cache[key] === undefined ? -1 : this.cache[key];
+  return this.cache[key] === undefined ? -1 : this.cache[key].value;
 };
 
 /**
